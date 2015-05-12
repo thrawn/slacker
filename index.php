a9e1af870c2c6a167974c52535559d91ed7ffd82
michael hoover <mike.hoover@gmail.com>
2015-05-12 11:38:15 -0700

    add the cats of course

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php
 header("Content-Type: text/plain");
 echo "Hello, world! Love Mike\n";
-echo ""
-echo '<img src="https://media3.giphy.com/media/1u1J2zMhv8Mlq/200_s.gif" />'
+echo "<br />";
+echo '<img src="https://media3.giphy.com/media/1u1J2zMhv8Mlq/200_s.gif" />';
