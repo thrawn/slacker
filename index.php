67f2f341165a58754a9ab4903297e6ee0c5f1b91
michael hoover <mike.hoover@gmail.com>
2015-05-12 12:04:17 -0700

    loakal cat

--- index.php
+++ index.php
@@ -2,4 +2,4 @@
 header("Content-Type: text/html");
 echo "Hello, world!\n";
 echo "<br />";
-echo '<img src="http://www.catster.com/files/styles/424_200sc/public/fields/20121113-catster-cat-in-box-video-hero.jpg" />';
+echo '<img src="20121113-catster-cat-in-box-video-hero.jpg" />';
