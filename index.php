1945fa2ef75658c024ddbb84cd31c080608a0b7b
michael hoover <mike.hoover@gmail.com>
2015-05-12 11:51:32 -0700

    html

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php
 header("Content-Type: text/html");
-echo "Hello, world! Love Mike\n";
-echo "\n";
+echo "Hello, world!\n";
+echo "<br />";
 echo '<img src="http://www.catster.com/files/styles/424_200sc/public/fields/20121113-catster-cat-in-box-video-hero.jpg" />';
