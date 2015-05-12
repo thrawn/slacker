6cef71b089b3b529c88d03a81b360a73ec312788
michael hoover <mike.hoover@gmail.com>
2015-05-12 11:43:39 -0700

    cat in box

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php
 header("Content-Type: text/plain");
 echo "Hello, world! Love Mike\n";
-echo "<br />";
-echo '<img src="https://media3.giphy.com/media/1u1J2zMhv8Mlq/200_s.gif" />';
+echo "\n";
+echo '<img src="http://www.catster.com/files/styles/424_200sc/public/fields/20121113-catster-cat-in-box-video-hero.jpg" />';
