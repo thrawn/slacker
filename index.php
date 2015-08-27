999da67b65422fed8b7260442e0c36024b3b3cdb
root <root@ip-10-77-29-240.us-west-2.compute.internal>
2015-08-27 20:44:00 +0000

    elb demo

--- index.php
+++ index.php
@@ -1,5 +1,6 @@
 <?php
 header("Content-Type: text/html");
 echo "Hello, world!\n";
+echo "Server: $_SERVER['SERVER_ADDR']";
 echo "<br />";
 echo '<img src="20121113-catster-cat-in-box-video-hero.jpg" />';
