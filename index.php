62e3cdd742b58ce4e0dec33d377b0b7a100737d7
root <root@ip-10-77-30-146.us-west-2.compute.internal>
2015-08-27 23:17:34 +0000

    add no cache headers

--- index.php
+++ index.php
@@ -2,8 +2,13 @@
 <html lang="en-US">
 <head>
 <title>Hello There</title>
+<meta http-equiv="Cache-Control" content="no-cache, no-store, must-revalidate" />
+<meta http-equiv="Pragma" content="no-cache" />
+<meta http-equiv="Expires" content="0" />
 </head>
 <body>
 <h3>Oh, Hello There</h3>
 <img src="20121113-catster-cat-in-box-video-hero.jpg" />
 </body>
+<pre>ip-10-77-30-146.us-west-2.compute.internal</pre>
+</html>
