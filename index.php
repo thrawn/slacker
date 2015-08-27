4838baa5be6628113b23e942319a073b8962ad83
root <root@ip-10-77-66-51.us-west-2.compute.internal>
2015-08-27 22:58:44 +0000

    stupid crap

--- index.php
+++ index.php
@@ -1,6 +1,9 @@
-<?php
-header("Content-Type: text/html");
-echo "Hello, world!\n";
-echo "Server: $_SERVER['SERVER_ADDR']\n";
-echo "<br />";
-echo '<img src="20121113-catster-cat-in-box-video-hero.jpg" />';
+<!DOCTYPE html>
+<html lang="en-US">
+<head>
+<title>Hello There</title>
+</head>
+<body>
+<h3>"Oh, Hello There"</h3>
+<img src="20121113-catster-cat-in-box-video-hero.jpg" />
+</body>
