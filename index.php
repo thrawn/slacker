465cc83132bf0fea357469c398021ef0db455b07
root <root@ip-10-77-75-117.us-west-2.compute.internal>
2015-08-27 20:48:04 +0000

    ok

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php
 header("Content-Type: text/html");
 echo "Hello, world!\n";
-echo "Server: $_SERVER['SERVER_ADDR']";
+echo "Server: $_SERVER['SERVER_ADDR']\n";
 echo "<br />";
 echo '<img src="20121113-catster-cat-in-box-video-hero.jpg" />';
